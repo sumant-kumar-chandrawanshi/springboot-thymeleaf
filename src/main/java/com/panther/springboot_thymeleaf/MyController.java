14a53eaba20ca07202e07565ba78d923ea69bb15
suamnt-kumar-chandrawanshi <sumantkumrchandrawanshi@gmail.com>
2026-05-04 10:19:18 +0530

    first commit

---
+++ src/main/java/com/panther/springboot_thymeleaf/MyController.java
@@ -0,0 +1,13 @@
+package com.panther.springboot_thymeleaf;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+	@RequestMapping("/test")
+	public String test() {
+		return "test";
+	}
+}
